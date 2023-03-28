30ebb3b6886a80e4b3c5109b0d560461b006f7a8
gav-cooper <gavin.cooper@smail.astate.edu>
2023-03-28 14:12:26 -0500

    testing

--- app/src/main/java/com/braden/restaurant/MainActivity.kt
+++ app/src/main/java/com/braden/restaurant/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var favoritesButton: Button
     private lateinit var randomButton: Button
     private lateinit var viewedButton: Button
+    // did it work
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
